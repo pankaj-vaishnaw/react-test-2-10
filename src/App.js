3cbf18072c9f93d0055c55e40f2550c5bc7cba97
Pankaj Vaishnav <97958495+pankaj-vaishnaw@users.noreply.github.com>
2022-10-02 22:07:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,48 @@
+
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className='folrr'>
+        <img src="/assets/download.png" className='fold'/>
+
+    <div className="App">
+      <div className='srch'>
+        <img src="/assets/windo.png" className='windo'/>
+   <input type="text" placeholder='type here to search'className='serachbox'></input>
+        </div>
+        <div className=' images'>
+
+        <img src="/assets/whatsapp.png" className='whats'/>
+        <img src="/assets/crom.png" className='crome'/>
+        <img src="/assets/set.png" className='set'/>
+        <img src="/assets/opra.png" className='opra'/>
+        <img src="/assets/fldr.png" className='folder'/>
+        </div>
+        <div className='gap-div'></div>
+      <div className='text'>
+        <img src="/assets/sun.png" className='sun'/>
+        <span>34 C Sunny   ^</span>
+        <img src="/assets/meet.png" className='meet'/>
+        <img src="/assets/audio.png" className='aud'/>
+        
+      </div>
+      <div className='date'>
+        <span>9:50 PM</span><br/>
+        <span>10/2/2022</span>
+        
+      </div>
+      {/* <div className='mes'>
+
+        <img src="/assets/msgs.png" className='mesg'/>
+      </div> */}
+
+     
+    </div>
+    </div>
+  );
+}
+
+export default App;
